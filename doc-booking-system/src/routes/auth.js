2694dfee93cf2913c1223a69ade577c529e8c4d2
Ntokozo31 <dubentokozo45@yahoo.com>
2024-12-22 13:11:17 +0200

    add profile route for viewing/updating profile and  logout route

--- doc-booking-system/src/routes/auth.js
+++ doc-booking-system/src/routes/auth.js
@@ -5,12 +5,25 @@ const router = express.Router();
 
 //Registration router
 router.get('/register', (req, res) => {
-    res.send('Registration page')
+    res.send('User registration successfully');
 });
 
 //Login route
 router.get('/login', (req, res) => {
-    res.send('Login page')
+    res.send('User login successfully');
+});
+
+//Logout route
+router.post('/logout', (req, res) => {
+    res.send('User logout successfully');
+});
+
+//Profile route (view/ update profile)
+router.get('/profile', (req, res) => {
+    res.send('User profile fatched successfully');
+});
+router.put('/profile', (req,res) => {
+    res.send('User successfully updated profile');
 });
 
 module.exports = router;
