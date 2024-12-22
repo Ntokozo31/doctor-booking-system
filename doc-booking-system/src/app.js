6d3b32f97a6759ebb0c81b44fe2e0d68d8bfbcda
Ntokozo31 <dubentokozo45@yahoo.com>
2024-12-22 14:12:10 +0200

    added appointment and doctors and mounted them to their specific routes

--- doc-booking-system/src/app.js
+++ doc-booking-system/src/app.js
@@ -5,12 +5,20 @@ const express = require('express');
 const app = express();
 
 const authRoutes = require('./routes/auth');
+const doctorRoutes = require('./routes/doctor');
+const appointmentRoutes = require('./routes/appointment');
 
 //Middleware for json parsing
 app.use(express.json());
 
 //Use the auth route
 app.use('/auth', authRoutes);
+
+//use the doctors routes
+app.use('/doctor', doctorRoutes);
+
+//use the appointment routes
+app.use('/appointment', appointmentRoutes);
 
 //Welcoming home page
 app.get('/', (req, res, next) => {
