b48e6210461514d34e8dad8848ed8511517f5c12
Ntokozo31 <dubentokozo45@yahoo.com>
2024-12-23 23:28:17 +0200

    Add validation in User registration route
    Added middleware to parse incoming json data
    Added validation for all the fields
    Added validation to check errors
    Extracted data from request
    checked if all are not missing

--- doc-booking-system/src/routes/auth.js
+++ doc-booking-system/src/routes/auth.js
@@ -4,9 +4,35 @@ const express = require('express');
 //initialize router
 const router = express.Router();
 
+// Middleware to parse incoming json data from request
+router.use(express.json());
+
+// Middleware specially for only this route
+router.use((req, res, next) => {
+    console.log('Auth route middleware is active');
+    next();
+});
+
 //Registration router
-router.post('/users/register', (req, res) => {
-    res.send('User registration successfully');
+router.post('/users/register', [
+    // Validate the fields
+    body('name').not().isEmpty().withMessage('Name is required'),
+    body('email').not().isEmpty().withMessage('Your email is required').isEmail().withMessage('Invalid eamil'),
+    body('password').not().isEmpty().withMessage('password is required').isLength({min: 6}).withMessage('Password must be atleast 6 characters'),
+    ], (req, res) => {
+        // Check validation errors
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        // Extract data from request
+        const {name, email, password} = req.body;
+
+        // Check if the fields are missing
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: 'Usename, Email and Password are required'});
+        }
+        res.send('User registration successful');
 });
 
 //Login route
