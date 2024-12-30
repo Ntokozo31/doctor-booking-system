bc56eea019dab0231c0f4a4a2996fa51c8972cae
Ntokozo31 <dubentokozo45@yahoo.com>
2024-12-30 16:12:07 +0200

    Add global cors, body parser
    added global cors and body parser
    Imported route for auth.js
    Added home route
    Added a route that will handle 404

--- doc-booking-system/src/controllers/authController.js
+++ doc-booking-system/src/controllers/authController.js
@@ -1,5 +1,5 @@
 // Import User model
-const User = require('../models/User');
+const User = require('../server/users');
 
 // Import bcrypt
 const bcrypt = require('bcrypt');
@@ -9,3 +9,15 @@ const jwt = require('jsonwebtoken');
 
 // Import validation
 const { validationResult } = require('express-validator');
+
+const getProfile = async (req, res) => {
+    const parsedId = parseInt(req.params.id);
+    if (!isNaN(parsedId))
+        return res.status(400).send('Bad Request');
+    const findUsers = User.find(user => user.id === parseInt(req.params.id))
+    if (!findUsers)
+        return res.status(400).send('Invalide User');
+    res.send(findUsers);
+}
+
+module.export = getProfile;
--- doc-booking-system/src/routes/auth.js
+++ doc-booking-system/src/routes/auth.js
@@ -2,25 +2,33 @@
 const express = require('express');
 
 // Import middlewares
-const { validateRegistration, validateLogin } = require('../middlewares/auth');
+//const { validateRegistration, validateLogin } = require('../middlewares/auth');
 
 // Import auth controller
-const { register, login, logout, getProfile } = require('../controllers/authController');
+//const { register, login, logout, getProfile } = require('../controllers/authController');
 
 // Create router
 const router = express.Router();
 
 // Registration router
-router.post('/register', register);
+router.post('/register', (req, res) => {
+    res.send('User registered successfully');
+});
 
 // Login router
-router.post('/login', login);
+router.post('/login', (req, res) => {
+    res.send('User logged in successfully');
+});
 
 // Logout router
-router.post('/logout', logout);
+router.get('/logout', (req, res) => {
+    res.send('User successfully logged out');
+});
 
 // Profile router
-router.get('/profile/:id', getProfile);
+router.get('/profile/:id', (req, res) => {
+    res.send('User profile successfully retrieved');
+});
 
-// Exports router
+// Export router
 module.exports = router;
