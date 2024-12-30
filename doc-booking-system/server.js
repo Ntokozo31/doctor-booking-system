65588be93eee79f9394d69fe8341cae016574730
Ntokozo31 <dubentokozo45@yahoo.com>
2024-12-30 16:20:46 +0200

    Add global cors, body parser
    Added global cors and body parser
    Import authRouter
    Added home route
    Added route that will be handling 404 routes which are undefined

--- doc-booking-system/server.js
+++ doc-booking-system/server.js
@@ -1,13 +1,44 @@
 // Import express
 const express = require('express');
 
-// Initialize the app
+
+//Import cors
+const cors = require('cors');
+
+// Import routes
+const authRouters = require('./src/routes/auth');
+
+// Initailize the app
 const app = express();
 
 // Port setup
 const PORT = process.env.PORT || 3000;
 
+// Global cors
+app.use(cors());
+
+// Body parser for incoming requests
+app.use(express.json());
+
+// Import routes
+const authRouter = require('./src/routes/auth');
+
+// Auth router to handle all the auth routes
+app.use('/api/auth', authRouter);
+
+// Home route
+app.get('/', (req, res) => {
+    res.send('Welcome to DocBook your number one booking system');
+});
+
+// The route that will handle 404, routes not found fallback
+app.use((req, res) => {
+    res.status(404).send('This route does not exist');
+})
 // Port listener
 app.listen(PORT, () => {
-    console.log(`Server is now running on port ${PORT}`)
-})
+    console.log(`Server is now running on port ${PORT}`);
+});
+
+// Export the app
+module.exports = app;
