2aebb85f78e8852ccd742b93a292eccd57211bc0
Ntokozo31 <dubentokozo45@yahoo.com>
2024-12-30 17:46:26 +0200

    Added clear documentation for all the routers

--- doc-booking-system/src/routes/auth.js
+++ doc-booking-system/src/routes/auth.js
@@ -5,21 +5,25 @@ const express = require('express');
 const router = express.Router();
 
 // Registration router
+// This router will be used to register user
 router.post('/register', (req, res) => {
     res.send('User registered successfully');
 });
 
 // Login router
+// This router will be used to login user
 router.post('/login', (req, res) => {
     res.send('User logged in successfully');
 });
 
 // Logout router
+// This router will be used to logout user
 router.get('/logout', (req, res) => {
     res.send('User successfully logged out');
 });
 
 // Profile router
+// This router will be used to get user profile
 router.get('/profile/:id', (req, res) => {
     res.send('User profile successfully retrieved');
 });
