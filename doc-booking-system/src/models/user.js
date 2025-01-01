4dd2ffebdbdcc19c2766d9f77f7bece4b33818fd
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-01 19:07:52 +0200

    Add email and passwoed in user schema
    Added lowercase is email
    Added minimum length in password

--- doc-booking-system/src/models/user.js
+++ doc-booking-system/src/models/user.js
@@ -5,20 +5,36 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+    //User name
     username: {
         type: String,
         required: true,
         unique: true,
         trim: true,
     },
+    // User email
     email: {
         type: String,
         required: true,
         unique: true,
+        lowercase: true,
     },
+    // User passsword
     password: {
         type: String,
         required: true,
-
+        minlength: 6,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
     }
-})
+});
