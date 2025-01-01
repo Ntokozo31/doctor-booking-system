769018b58b7c9c2f98cf60982b8db56f86d39ead
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-01 03:36:58 +0200

    Add new doctor
    Conneccted to mogodb
    Added new doctor
    Inserted new doctor in our database
    Closed connection

--- doc-booking-system/src/config/db.js
+++ doc-booking-system/src/config/db.js
@@ -2,7 +2,37 @@
 const mongoose = require('mongoose');
 
 // Import doctor model
-const Docs = require('/..models/doctor');
+const Docs = require('/src/models/doctor');
 
 // Connect to mongoDB
-mongoose.connect('mongodb://localhost:27017/Doctors', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+    'mongodb://localhost:27017/Doctors',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true, })
+    .then(() => console.log('Successfully connected to MongoDb'))
+    .catch(err => console.log('Sorry, could not connect to MongoDb', err));
+
+// Add new doctor
+const addDoctor = [
+    {
+        name: 'Dr Ntokozo Dube',
+        email: 'ntokozod@gamil.com',
+        speciality: 'Cardiologist',
+        qualification: 'MBChB',
+        experiance: '8 years',
+        availability: {
+            days: ['Monday', 'Tuesday', 'Friday'],
+            startTime: '08:00',
+            endTime: '17:00',
+        },
+        Location: 'Durban',
+    }
+]
+
+// Insert new doctors to system
+Docs.insertOne(addDoctor)
+    .then(() => console.log('Doctor successfully added'))
+    .catch(err => console.log('Sorry, could not add doctor', err));
+
+// Close connection
+mongoose.connection.close();
