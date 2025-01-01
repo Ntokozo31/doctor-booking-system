53edc6a19c70d2b4341789652fb51dd20641808b
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-01 15:03:22 +0200

    Add User schema
    Imported express and mongoose
    Created new user schema with username, email and password

---
+++ doc-booking-system/src/models/user.js
@@ -0,0 +1,24 @@
+// Import express
+const express = require('express');
+
+// Import mongoose
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        
+    }
+})
