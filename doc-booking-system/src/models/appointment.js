c122ffbce52fb93c1f05876bc3aba76490505653
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-01 19:30:11 +0200

    Add appointment model
    Created appointment schema
    Added user id, doctor id, createdAt,appointment date, time and status
    Added cancellation in the appointment schema

---
+++ doc-booking-system/src/models/appointment.js
@@ -0,0 +1,51 @@
+// Import express
+const express = require('express');
+
+// Import mongoose
+const mongoose = require('mongoose');
+
+const AppointmentSchema = new mongoose.Schema({
+    // User id
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    // Doctor id
+    doctorId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Doctor',
+        required: true,
+    },
+    // Appointment date
+    date: {
+        type: Date,
+        required: true,
+    },
+    // Appointment time
+    time: {
+        type: String,
+        required: true,
+    },
+    // Appointment status
+    status: {
+        type: String,
+        enum: ['pending', 'approved', 'cancelled'],
+        default: 'pending',
+    },
+    // createdAt
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    // cancell appointment
+    cancelBy: {
+        type: String,
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+
+});
+
+// Export module
+module.exports = mongoose.model('Appointment', AppointmentSchema);
