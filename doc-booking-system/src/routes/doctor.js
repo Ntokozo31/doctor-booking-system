4e14c1a15ddc098c5d660ac968a79817f52d671e
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-03 10:53:34 +0200

    Add get all doctors and get doctor by name
    Rectieve all doctors from database
    Retrieve single doctor by name in database

--- doc-booking-system/src/routes/doctor.js
+++ doc-booking-system/src/routes/doctor.js
@@ -8,9 +8,11 @@ const router = express.Router();
 const { getDb } = require('../config/db');
 
 // Get all doctors
-// This route will be used to get all doctors
+// This route will be used to retrieve all doctors in our database
 router.get('/all/docs', (req, res) => {
-    // Get all doctors from the database
+    // Retrieve all doctors
+    // We get our doctors by name, speciality and location
+    // If our server has an error we return statusCode of 500
     const db = getDb();
     db.collection('doctors').find({}, {projection: { name: 1, speciality: 1, location: 1} }).toArray()
         .then(doctors => {
@@ -23,10 +25,26 @@ router.get('/all/docs', (req, res) => {
     //res.send('All doctors successfully retrieved');
 });
 
-// Get doctor by id only
-// This route will be used to get doctor by id
-router.get('/doc/:id', (req, res) => {
-    res.send('Doctor successfully retrieved');
+// Get doctor by Name only
+// This route will be used to get doctor by name
+router.get('/doc/:name', (req, res) => {
+    // Get single doctor by name
+    // Get more info about that particulater doctor (name, location, availability...)
+    // If the doctor does not exist we ruturn statusCode 400
+    // If it a server error we return statusCode 500
+    const db = getDb();
+    const docName = req.params.name;
+    db.collection('doctors').findOne({name: docName})
+        .then((doctors) => {
+            if (!doctors) {
+                return res.status(404).json({ message: 'This Doctor cannot be found'})
+            }
+            res.send(doctors)
+        }) .catch(err => {
+            console.log(err);
+            res.status(500).send('Sorry an error occured');
+        })
+    //res.send('Doctor successfully retrieved');
 });
 
 // New doctor to the system
