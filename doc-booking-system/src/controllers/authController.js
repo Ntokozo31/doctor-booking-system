99127e9fd637a5c262238de5bf41f2f75157c907
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-03 22:33:03 +0200

    Add authentication controller
    Added user to database
    Added validation fields
    Added a checker to check if the user already exist or not
    Adds error handler to handle internal error

--- doc-booking-system/src/controllers/authController.js
+++ doc-booking-system/src/controllers/authController.js
@@ -1,23 +1,32 @@
-// Import User model
-const User = require('../server/users');
+// Import getDb from db
+const { getDb } = require('../config/db');
 
-// Import bcrypt
-const bcrypt = require('bcrypt');
+// Register user and save to database
+const register = async (req, res) => {
+    try {
+        // Extract username, email and password
+        const { username, email, password } = req.body;
+        // Validate the fields
+        if (!username || !email || !password) {
+            return res.status(400).send('OOPS all fields are required')
+        }
+        const db = getDb();
+        // Check if the user already exist
+        // If the user exist we return statusCode of 400
+        const existUser = await db.collection('users').findOne({email})
+        if (existUser) {
+            return res.status(400).send('OOPS sorry user with this email already exit');
+        }
+        // Add new user to database
+        const newUser = { username, email, password}
+        await db.collection('users').insertOne(newUser);
+        res.status(201).send({ message: `Congratulations ${username} your registraion was successful` });
+    // Internal server error problem we return a statusCode of 500
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Eish sorry an internal error server occurred');
+    };
+};
 
-// Import jsonwebtoken
-const jwt = require('jsonwebtoken');
-
-// Import validation
-const { validationResult } = require('express-validator');
-
-const getProfile = async (req, res) => {
-    const parsedId = parseInt(req.params.id);
-    if (!isNaN(parsedId))
-        return res.status(400).send('Bad Request');
-    const findUsers = User.find(user => user.id === parseInt(req.params.id))
-    if (!findUsers)
-        return res.status(400).send('Invalide User');
-    res.send(findUsers);
-}
-
-module.export = getProfile;
+// Exports register
+module.exports = { register };
