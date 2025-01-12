b574a3cd81134642c7bdb08e5eb0bf8d559e9e05
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-12 18:49:47 +0200

    Add connection to mongo database
    Added connection to mongoDb using mongoClient module
    Imported dotenv to utilize mongoClient string in this file

--- doc-booking-system/src/config/db.js
+++ doc-booking-system/src/config/db.js
@@ -8,37 +8,16 @@ require('dotenv').config();
 const Docotor = require('../models/doctor');
 
 // Connect to mongoDB
-mongoose.connect('mongodb://localhost:27017/Doctors')
-    .then(() => {
-        console.log('Successfully connected to MongoDb')
-
-        // Add new doctor
-        const addDoctor = [
-            {
-                name: 'Dr Jane Doe',
-                email: 'janedoe@gmail.com',
-                speciality: 'Cardiologist',
-                qualification: 'MBChB',
-                availability: {
-                    days: ['Monday', 'Tuesday', 'Friday'],
-                    startTime: '08:00',
-                    endTime: '17:00',
-                },
-                location: 'Durban',
-                experience: '8 years'
-            }
-        ];
-
-        // Insert new doctors to system
-        return Docotor.insertMany(addDoctor);
-    })
-    .then(() => {
-        console.log('Doctor successfully added')
-        // close connetion
-        mongoose.connection.close();
-    })
-    .catch(err => {
-        console.log('Sorry, could not add doctor', err)
-        // Close connection
-        mongoose.connection.close();
-    });
+module.exports = {
+    connectToDb: (callback) => {
+        MongoClient.connect(uri)
+            .then((client) => {
+                dbConnection = client.db()
+                return callback();
+            })
+            .catch((err) => {
+                console.log('Sorry an error occurred while connecting to the database', err)
+            })
+    },
+    getDb: () => dbConnection
+};
