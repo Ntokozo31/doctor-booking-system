552b50eadbd583e5afc39830868480b784eea5ae
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-23 10:54:48 +0200

    Add javascript bookings
    Added javascript fetch with POST method for API requests

---
+++ doc-booking-system/public/bookings.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('.appointment-form').addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const appointmentData = {
+            speciality: document.getElementById('specialty').value,
+            location: document.getElementById('location').value,
+            days: document.getElementById('days').value,
+            time: document.getElementById('time').value,
+            message: document.getElementById('showMessage').value,
+        }
+
+        try {
+            const response = await fetch('/api/appointment/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(appointmentData),
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                showMessage.textContent = data.message;
+                showMessage.style.color = 'green';
+                setTimeout(function () {
+                    window.location.href = 'home.html'
+                }, 2000);
+            } else if (response.status === 401) {
+                showMessage.textContent = data.message;
+                showMessage.style.color = 'red';
+            } else if (response.status === 400) {
+                showMessage.textContent = data.message;
+                showMessage.style.color = 'red';
+            } else {
+                showMessage.textContent = data.message;
+                showMessage.style.color = 'red';
+            }
+        } catch (error) {
+            showMessage.textContent = 'Sorry something went wrong';
+            showMessage.style.color = 'red';
+        }
+    });
+});
