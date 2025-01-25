e3217dc9afa242576f29c7b0e6d0736c407c82d8
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-25 14:11:19 +0200

    Change callback function and route
    Changed callback function to get doctor by location instead of getting
    doctor by name
    Added new parameter of location

--- doc-booking-system/src/routes/doctor.js
+++ doc-booking-system/src/routes/doctor.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const { getDb } = require('../config/db');
 
 // Import allDocotors
-const { allDoctors, doctorByName } = require('../controllers/doctorController');
+const { allDoctors, doctorByLocation } = require('../controllers/doctorController');
 
 // Get all doctors
 // This route will be used to retrieve all doctors in our database
@@ -16,7 +16,7 @@ router.get('/all/docs', allDoctors);
 
 // Get doctor by Name only
 // This route will be used to get doctor by name
-router.get('/doc/:name', doctorByName);
+router.get('/doc/:location', doctorByLocation);
 
 // Export router
 module.exports = router;
