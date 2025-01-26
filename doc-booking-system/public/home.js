140d1aec975af8e444cae620d324606a6a4d00ba
Ntokozo31 <dubentokozo45@yahoo.com>
2025-01-26 02:27:16 +0200

    Added home navigation toggle

---
+++ doc-booking-system/public/home.js
@@ -0,0 +1,7 @@
+// Nav menu toggle
+// Get query selector for nav-list
+// Toggle the class show on nav-list
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-list');
+    navLinks.classList.toggle('show');
+}
