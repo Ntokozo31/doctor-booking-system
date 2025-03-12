ff7c883478da343183a6bd3663163909ffe4a9d5
Ntokozo31 <dubentokozo45@yahoo.com>
2025-03-12 11:41:10 +0200

    Add confirmation email function
    Added confirmation function that will send an email to user
    Added text content that will be rended when user recieved the email with
    all the details of the appointment
    Added subject in of the email

--- doc-booking-system/mail.js
+++ doc-booking-system/mail.js
@@ -18,4 +18,30 @@ const transporter = nodemailer.createTransport({
         user: MAILTRAP_USERNAME,
         password: MAILTRAP_PASSWORD
     }
-})
+});
+
+// Function to send confirmation email to user
+// We pass in the userName, userEmail and appointmentDetails
+// We create a mailOptions object with from, to, subject, text and html
+const sendConfirmationEmail = async (userName, userEmail, appointmentDetails) => {
+    const mailOptions = {
+        from: '"Doctor Booking System" docbook77@gmail.com',
+        to: userEmail,
+        subject: 'Appointment Confirmation',
+        text: `Hello ${userName}, your appointment has been booked successfully. Your appointment details are: ${appointmentDetails}`,
+        html: `<p>Hellow <strong>${userName}</strong>,</p><p>Your appointment has been confirmed for ${appointmentDetails}</p>`
+    };
+
+    try {
+        // Send email to user
+        // We await the transporter to send the mail
+        // If the mail is sent we console log the message id
+        // If there is an error we console log the error
+        const info = await transporter.sendMail(mailOptions);
+        console.log('Message sent: ', info.messageId);
+    } catch (error) {
+        console.error('Error sending mail:', error);
+    }
+}
+
+module.exports = sendConfirmationEmail;
