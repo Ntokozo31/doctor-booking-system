3772042b96558fb5de057a07753ce05fc4008870
Ntokozo31 <dubentokozo45@yahoo.com>
2025-05-28 14:06:03 +0200

    feat: Add new middleware route function
    Added authenticateUser function to handle all user authentication in
    booking appointment and in getting all appointmnets for user

--- doc-booking-system/src/routes/appointment.js
+++ doc-booking-system/src/routes/appointment.js
@@ -1,21 +1,26 @@
 // Import express
 const express = require('express');
+
+// Import appointment controller
 const {
     bookAppointment,
     allAppointment,
     availableSlots
 } = require('../controllers/appointmentController');
 
+// Import authenticateUser middleware
+const authenticateUser = require('../middlewares/authMiddleware');
+
 // Initialize the router
 const router = express.Router();
 
 // Create an appointment
 // This route will be used to create an appointment
-router.post('/create', bookAppointment);
+router.post('/create', authenticateUser, bookAppointment);
 
 // Get all appointments for that user
 // This route will be used to get all appointment for user
-router.get('/all/', allAppointment);
+router.get('/all/', authenticateUser, allAppointment);
 
 // Check available slots
 // This route will be used to ckeck available slots
