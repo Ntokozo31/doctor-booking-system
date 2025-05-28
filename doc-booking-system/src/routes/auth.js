cf1296921e0f569d4205fcffa3301701328a52a1
Ntokozo31 <dubentokozo45@yahoo.com>
2025-05-28 12:47:45 +0200

    feat: Added new middleware routes
    Added new middleware routes for myProfile, updateUserProfile and delete
    to improve code organization and use them as middleware routes

--- doc-booking-system/src/routes/auth.js
+++ doc-booking-system/src/routes/auth.js
@@ -4,7 +4,10 @@ const express = require('express');
 // Initialize router
 const router = express.Router();
 
-// Import database
+// Import authenticateUser middleware
+const authenticateUser = require('../middlewares/authMiddleware');
+
+// Import authController
 const { register, userLogin, myProfile, userLogout, updateUserProfile, deleteUserProfile } = require('../controllers/authController');
 
 // Registration router
@@ -22,15 +25,15 @@ router.post('/logout', userLogout);
 
 // Profile router
 // This router will be used to get user profile
-router.get('/profile/', myProfile);
+router.get('/profile/', authenticateUser, myProfile);
 
 // Update profile router
-// This router will be used to update user profile
-router.put('/profile/update', updateUserProfile);
+// This router will be used to update user profile.
+router.put('/profile/update', authenticateUser, updateUserProfile);
 
 // Delete router
 // This router will be used to delete user profile
-router.delete('/profile/delete', deleteUserProfile)
+router.delete('/profile/delete', authenticateUser, deleteUserProfile)
 
 // Export router
 module.exports = router;
