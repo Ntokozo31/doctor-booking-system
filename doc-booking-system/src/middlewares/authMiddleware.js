4ef2d55501edbebad996233d52e47f4bb75723dc
Ntokozo31 <dubentokozo45@yahoo.com>
2025-05-28 15:13:51 +0200

    chore: add comments to explain authentication middleware

--- doc-booking-system/src/middlewares/authMiddleware.js
+++ doc-booking-system/src/middlewares/authMiddleware.js
@@ -1,15 +1,23 @@
+// Import jsonwebtoken
 const jwt = require('jsonwebtoken');
 
+// Import dotenv to manage environment variables
 require('dotenv').config();
 
+// Initialize JWT_SECRET from environment variables
 const JWT_SECRET = process.env.JWT_SECRET;
 
+// Middleware to authenticate user using JWT
 const authenticateUser = (req, res, next) => {
     const token = req.cookies.token;
+    // Check if token is provided
+    // If no token is provided we return a statusCode of 401
     if(!token) {
         return res.status(401).json({ message: 'No token provided, Please login and try agian' });
     }
     try {
+        // Verify the token using jwt.verify
+        // If token is invalid or expired, jwt.verify will throw an error
         const decoded = jwt.verify(token, JWT_SECRET);
         req.userId = decoded.userId;
         next();    
@@ -18,4 +26,5 @@ const authenticateUser = (req, res, next) => {
     }
 };
 
+// Module exports the authenticateUser middleware
 module.exports = authenticateUser;
