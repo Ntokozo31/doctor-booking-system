b5500203505f459762bc9d749c0185aaca6928dd
Ntokozo31 <dubentokozo45@yahoo.com>
2025-05-28 15:28:09 +0200

    feat: add hamburger for nav links and make it responsive

--- doc-booking-system/public/signup-login.js
+++ doc-booking-system/public/signup-login.js
@@ -122,3 +122,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+const hamburgerBtn = document.getElementById('hamburger-btn');
+    const navLinks = document.querySelector('.nav-links');
+    hamburgerBtn.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
